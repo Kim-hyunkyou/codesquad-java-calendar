22dc62fdbab8033a37343fcddbd9670f5d1514c6
HyunKyouKim <HyunKyouKim@LAPTOP-AFVD85QJ>
2020-09-19 16:24:40 +0900

    Update for Repeat

--- src/kyous/calendar/MaxMonth.java
+++ src/kyous/calendar/MaxMonth.java
@@ -7,13 +7,21 @@ public class MaxMonth {
 
 		Scanner scan = new Scanner(System.in);
 
-		System.out.print("몇월을 확인 하시겠습니까?");
-		int month = scan.nextInt();
 
-		int[] maxDays = {31,28,31,30,31,31,30,31,30,31};
+		System.out.print("반복횟수를 입력하세요.");
+		int repeat = scan.nextInt();
+		for (int i = 0; i < repeat; i++) {
+			System.out.print("몇월을 확인 하시겠습니까?");
+			int month = scan.nextInt();
+			
+			int[] maxDays = { 31, 28, 31, 30, 31, 31, 30, 31, 30, 31 };
+			
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, maxDays[month - 1]);
+			
+		}
 		
-		System.out.printf("%d월은 %d일까지 있습니다. \n",month,maxDays[month-1]);
 
+		System.out.println("Finished~");
 		scan.close();
 
 	}
